c364c637d69f1dcffd843b5056ac2209d22ed1ef
Rawllings <frawllings@example.com>
2023-02-07 21:07:47 -0800

    fifth

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,29 @@ import uuid
 import models
 
 
+
 class BaseModel():
     """Base Model
     """
-    def __init__(self):
-        """Initialize a new BaseModel."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args: not used
+            **kwargs:
+        """
 
         self.id = uuid.uuid4()
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.datetime.strptime("%Y-%m-%dT%H:%M:%S.%f", value)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """return the print str representation of th BaseModel
@@ -27,6 +41,7 @@ class BaseModel():
         """Updates the update_at with current datetime
         """
         self.updated_at = datetime.datetime.now()
+        models.storage.save(self)
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance."""
